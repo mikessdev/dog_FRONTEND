31b4b99c7cac1731f64002a4344a3c05f6ffedf6
mikessdev <mikesaints.2000@gmail.com>
2024-01-10 20:23:19 -0300

    feat: adding home title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,29 +12,44 @@ onMounted(async () => {
 
 <template>
   <main class="home-view">
-    <ul class="list-of-breed">
-      <li
-        class="breed"
-        v-for="(breed, key) in breedStore.listOFBreeds"
-        :key="key"
-      >
-        <Card :breed="key" :sub-breed="breed" />
-      </li>
-    </ul>
+    <h1 class="home-title">Dog API</h1>
+    <div class="list-of-breed-container">
+      <ul class="list-of-breed">
+        <li
+          class="breed"
+          v-for="(breed, key) in breedStore.listOFBreeds"
+          :key="key"
+        >
+          <Card :breed="key" :sub-breed="breed" />
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
 <style scoped>
 .home-view {
-  background-color: #eaeae8;
   display: flex;
+  background-color: #eaeae8;
+  flex-direction: column;
+}
+.home-title {
+  text-align: center;
+  font-size: 40px;
+  font-weight: 600;
+  margin: 40px;
+}
+
+.list-of-breed-container {
+  display: flex;
+  width: 100%;
   justify-content: center;
 }
 .list-of-breed {
-  width: 400px;
-}
-
-.breed {
-  margin: 10px 0px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+  max-width: 1200px;
 }
 </style>
