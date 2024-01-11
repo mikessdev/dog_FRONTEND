a9908d9451950090faa8c06638512889d26bde61
mikessdev <mikesaints.2000@gmail.com>
2024-01-11 18:28:08 -0300

    feat: .

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import DogsView from "../views/DogsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/dogs",
+      name: "dogs",
+      component: DogsView,
     },
   ],
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { useBreedStore } from "@/stores/breedStore";
 import { ref, onMounted } from "vue";
-import Card from "@/components/Card.vue";
 import Pagination from "@/components/Pagination.vue";
 
-const numberItemsByPage = 6;
+const numberItemsByPage = 10;
 
 const breedStore = useBreedStore();
 
@@ -13,19 +12,19 @@ const pageLength = ref<number>(0);
 
 onMounted(async () => {
   await breedStore.getAllBreeds();
-  listOnDisplay.value = breedStore.listOFBreeds.slice(0, 6);
+  listOnDisplay.value = breedStore.listOFBreeds.slice(0, numberItemsByPage);
   setupPagination();
 });
 
 const setupPagination = () => {
-  pageLength.value = Number(
-    (breedStore.listOFBreeds.length / numberItemsByPage).toFixed()
+  pageLength.value = Math.ceil(
+    Number(breedStore.listOFBreeds.length / numberItemsByPage)
   );
 };
 
 const reloadPageContent = (number: number) => {
-  const last = 6 * number;
-  const first = last - 6;
+  const last = numberItemsByPage * number;
+  const first = last - numberItemsByPage;
   listOnDisplay.value = breedStore.listOFBreeds.slice(first, last);
 };
 </script>
@@ -34,18 +33,36 @@ const reloadPageContent = (number: number) => {
   <main class="home-view">
     <div class="home-header">
       <h1 class="home-title">Dog API</h1>
-      <div class="home-car">
-        <span>Carrinho</span>
-        <img src="../assets/doguinho.png" width="80" />
-      </div>
+      <router-link to="/dogs">
+        <div class="home-car">
+          <span>Carrinho</span>
+          <img src="../assets/doguinho.png" width="80" />
+        </div>
+      </router-link>
     </div>
-    <div class="list-of-breed-container">
-      <ul class="list-of-breed">
-        <li class="breed" v-for="(breed, index) in listOnDisplay" :key="index">
-          <Card :breed="breed" />
-        </li>
-      </ul>
-    </div>
+
+    <table class="table-breed">
+      <thead class="">
+        <tr>
+          <th>Raça</th>
+          <th>Sub Raças</th>
+          <th>Cor</th>
+          <th>Tamanho</th>
+          <th>Apelido</th>
+          <th>Idade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(breed, index) in listOnDisplay" :key="index">
+          <td class="apply-border">{{ breed }}</td>
+          <td>{{ breed }}</td>
+          <td>{{ "" }}</td>
+          <td>{{ "" }}</td>
+          <td>{{ "" }}</td>
+          <td>{{ "" }}</td>
+        </tr>
+      </tbody>
+    </table>
 
     <Pagination @click="(e) => reloadPageContent(e)" :pageAmount="pageLength" />
   </main>
@@ -78,6 +95,37 @@ const reloadPageContent = (number: number) => {
   font-weight: 600;
   font-size: 20px;
   cursor: pointer;
+  color: #31394d;
+}
+
+.table-breed {
+  margin: 0 auto;
+  width: 90%;
+}
+.table-breed thead {
+  background-color: #31394d;
+  color: #e2e3ea;
+  padding: 10px;
+}
+
+.table-breed thead tr th {
+  padding: 10px;
+  border-radius: 4px;
+}
+
+.table-breed tbody tr {
+  transition: transform 0.2s;
+  background-color: rgb(189, 189, 189);
+}
+
+.table-breed tbody tr:hover {
+  transform: scale(1.05);
+  background-color: rgb(112, 112, 112);
+  color: white;
+}
+.table-breed td {
+  padding: 10px;
+  border-radius: 4px;
 }
 
 .list-of-breed-container {
